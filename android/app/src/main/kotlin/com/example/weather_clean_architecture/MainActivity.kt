5e5474e60816ee3b34e8da78898aefda0d1a943b
AmmarAgeeza <ammarfathy516@gmail.com>
2023-01-10 17:49:40 +0200

    initial and domain layer

---
+++ android/app/src/main/kotlin/com/example/weather_clean_architecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weather_clean_architecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/domain/usecases/get_weather_by_country.dart
@@ -0,0 +1,13 @@
+import 'package:weather_clean_architecture/domain/entities/weather.dart';
+
+import '../repository/weather_repository.dart';
+
+class GetWeatherByCountryName {
+  final WeatherRepository repository;
+
+  GetWeatherByCountryName(this.repository);
+
+  Future<Weather> execute(String cityName) async {
+    return await repository.getWeatherByCityName(cityName);
+  }
+}
